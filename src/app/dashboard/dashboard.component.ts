99051d17376425a2accb04ff36dde377daa8e40d
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2024-12-04 09:38:43 +0200

    making and using reusable component - dashboard-item.component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficComponent } from './traffic/traffic.component';
 import { TicketsComponent } from "./tickets/tickets.component";
+import {DashboardItemComponent} from "./dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
